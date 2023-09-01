2746cad3c6411f49b62d8cf336309fd59a01a128
amitkumarthakur9 <amit.thakur@fundexpertfintech.com>
2023-09-01 16:55:02 +0530

    sign with google on development build done

---
+++ src/screens/Test.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import "expo-dev-client";
+import { View, Text, Button } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
+
+export default Test = () => {
+  GoogleSignin.configure({
+    webClientId:
+      "707482261977-29se785gg5o3l51r7vnr6cp0pslvgh1f.apps.googleusercontent.com",
+  });
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await auth().signOut();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <GoogleSigninButton onPress={onGoogleButtonPress} />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+      <Button title="Sign Out" onPress={signOut} />
+    </View>
+  );
+};
