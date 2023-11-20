6af48268d149b1fb3a33de9932154d67a24d08a6
amitkumarthakur9 <amit.thakur@fundexpertfintech.com>
2023-11-20 15:52:37 +0530

    replaced fundexpert with staging url

--- src/api/services/apiClient.js
+++ src/api/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://www.fundexpert.in"; // Replace with your API's base URL
+const baseURL = "https://staging.fundexpert.in"; // Replace with your API's base URL
 
 const apiClient = axios.create({
   baseURL,
