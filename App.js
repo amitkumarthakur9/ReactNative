79880f598146b175643ca35c58f813e1e6a858dd
amitkumarthakur9 <amit.thakur@fundexpertfintech.com>
2023-11-26 23:05:18 +0530

    changes

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Splash"
+        initialRouteName="Dashboard"
         screenOptions={{
           headerShown: false,
         }}
