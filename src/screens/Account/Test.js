2b285d07258eab448d6f8f354b40cd869bfb53a7
amitkumarthakur9 <amit.thakur@fundexpertfintech.com>
2024-02-27 17:25:04 +0530

    changes

--- src/screens/Account/Test.js
+++ src/screens/Account/Test.js
@@ -1,21 +1,21 @@
-// import * as LocalAuthentication from "expo-local-authentication";
+import * as LocalAuthentication from "expo-local-authentication";
 
-// export default Test = async () => {
-//   const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
+export default Test = async () => {
+  const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
 
-//   if (isBiometricAvailable) {
-//     // Authenticate using biometrics
-//     const result = await LocalAuthentication.authenticateAsync();
+  if (isBiometricAvailable) {
+    // Authenticate using biometrics
+    const result = await LocalAuthentication.authenticateAsync();
 
-//     if (result.success) {
-//       // Biometric authentication successful
-//       console.log("Biometric authentication successful");
-//     } else {
-//       // Biometric authentication failed or user cancelled
-//       console.log("Biometric authentication failed or cancelled");
-//     }
-//   } else {
-//     // Biometric authentication is not available on the device
-//     console.log("Biometric authentication is not available on this device");
-//   }
-// };
+    if (result.success) {
+      // Biometric authentication successful
+      console.log("Biometric authentication successful");
+    } else {
+      // Biometric authentication failed or user cancelled
+      console.log("Biometric authentication failed or cancelled");
+    }
+  } else {
+    // Biometric authentication is not available on the device
+    console.log("Biometric authentication is not available on this device");
+  }
+};
